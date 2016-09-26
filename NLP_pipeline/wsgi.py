e2ddf013e06a0b090dcaaba42b73202ffe316852
unknown <>
2016-09-26 13:55:14 +0800

    APP "pipeline" has been created and registered

---
+++ NLP_pipeline/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for NLP_pipeline project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NLP_pipeline.settings")
+
+application = get_wsgi_application()
