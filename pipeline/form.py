d4e9f6d29fb179758ff8b48c08a1a8e4ae16f027
linsk <vn512903@hotmail.com>
2016-09-27 16:34:49 +0800

    remove blank argument

--- pipeline/form.py
+++ pipeline/form.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class DemoForm(forms.Form):
-	content = forms.CharField(max_length=1000,blank=False)
+	content = forms.CharField(max_length=1000)
 
 class ConsultForm(forms.Form):
 	user = forms.CharField(max_length=20)
