bb4a95c856fc5a60bb5b6a089bb47d4412f46710
linsk <vn512903@hotmail.com>
2016-09-27 17:22:44 +0800

    Create Consult model object and register in admin

--- pipeline/admin.py
+++ pipeline/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+import sys
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+from .models import Consult
 
 # Register your models here.
+admin.site.register(Consult)
--- pipeline/models.py
+++ pipeline/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Consult(models.Model):
+	name = models.CharField(max_length=20)
+	email = models.EmailField(max_length=30)
+	message = models.CharField(max_length=200)
