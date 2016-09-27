8efbeda514f5a8f412784888ded0f56e6b55e384
linsk <vn512903@hotmail.com>
2016-09-27 10:23:33 +0800

    Create consult form

--- pipeline/form.py
+++ pipeline/form.py
@@ -1,4 +1,9 @@
 from django import forms
 
 class DemoForm(forms.Form):
-    content = forms.CharField(max_length=1000,blank=False)
+	content = forms.CharField(max_length=1000,blank=False)
+
+class ConsultForm(forms.Form):
+	user = forms.CharField(max_length=20)
+	email = forms.EmailField(max_length=30)
+	message = forms.CharField(max_length=200)
